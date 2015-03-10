5cfa062b7acb44c9684523062148ffadba1fec00
Max Plomer <maxplomer@gmail.com>
2015-03-10 13:22:12 -0400

    add static_pages#home action to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 AuthDemo::Application.routes.draw do
+  root to: 'static_pages#home'
+
   resource :session, only: [:create, :destroy, :new]
   resource :user, only: [:create, :new, :show] do
     resource :counter, only: [:update]
